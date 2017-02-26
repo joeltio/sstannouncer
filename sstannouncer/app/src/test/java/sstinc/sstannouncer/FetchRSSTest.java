91557c77fd54979002b5a4f3e37598326240352a
MoonCheesez <joeltiojinhon@gmail.com>
2017-02-27 07:33:22 +0800

    Added test to check if XML is blogger's

--- sstannouncer/app/src/test/java/sstinc/sstannouncer/FetchRSSTest.java
+++ sstannouncer/app/src/test/java/sstinc/sstannouncer/FetchRSSTest.java
@@ -5,6 +5,7 @@ import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
+import java.io.IOException;
 import java.io.StringReader;
 
 import static org.junit.Assert.*;
@@ -27,6 +28,26 @@ public class FetchRSSTest {
 
         XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
         XmlPullParser parser = factory.newPullParser();
-        parser.setInput(new StringReader(rssFeed.getRawXML));
+        parser.setInput(new StringReader(rssFeed.getRawXML()));
+    }
+
+    @Test
+    public void rss_isBlogger() throws XmlPullParserException, IOException {
+        RSSFeed rssFeed = new RSSFeed();
+        RSSFeed.fetchRSS();
+
+        XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+        XmlPullParser parser = factory.newPullParser();
+        parser.setInput(new StringReader(rssFeed.getRawXML()));
+
+        int eventType = parser.getEventType();
+        while (eventType != XmlPullParser.END_DOCUMENT) {
+            if (eventType == XmlPullParser.START_TAG && parser.getName().equals("id")) {
+                parser.next();
+                assertTrue(parser.getText().startsWith("tag:blogger.com"));
+                break;
+            }
+            parser.nextTag();
+        }
     }
 }
