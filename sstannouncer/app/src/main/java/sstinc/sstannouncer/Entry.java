d75887372f5279128b54e4b6ee30abab2222176c
MoonCheesez <joeltiojinhon@gmail.com>
2017-03-02 22:33:08 +0800

    Added empty methods.

---
+++ sstannouncer/app/src/main/java/sstinc/sstannouncer/Entry.java
@@ -0,0 +1,31 @@
+package sstinc.sstannouncer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Entry {
+    public String getId() {
+        return "";
+    }
+    public String getPublished() {
+        return "";
+    }
+    public String getLastUpdated() {
+        return "";
+    }
+    public List<String> getCategories() {
+        return new ArrayList<>();
+    }
+    public String getAuthorName() {
+        return "";
+    }
+    public String getBloggerLink() {
+        return "";
+    }
+    public String getTitle() {
+        return "";
+    }
+    public String getContent() {
+        return "";
+    }
+}
