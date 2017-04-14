a83bef8e7fb685f9430b264f0fd9049094c746ab
MoonCheesez <joeltiojinhon@gmail.com>
2017-04-14 10:16:19 +0800

    Added body for XML to fetch XML and store it.

--- sstannouncer/app/src/main/java/sstinc/sstannouncer/Feed/XML.java
+++ sstannouncer/app/src/main/java/sstinc/sstannouncer/Feed/XML.java
@@ -1,17 +1,30 @@
 package sstinc.sstannouncer.Feed;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class XML {
-    String rawXml;
+    private String rawXml;
     public XML() {
         this.rawXml = "";
     }
 
-    public void fetch(String url) {
+    private String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
 
+    public void fetch(String urlString) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+        urlConnection.connect();
+        this.rawXml = convertStreamToString(urlConnection.getInputStream());
     }
 
     public String getRawXML() {
-        return "";
+        return this.rawXml;
     }
 
     public Object xpath(String xpath) {
