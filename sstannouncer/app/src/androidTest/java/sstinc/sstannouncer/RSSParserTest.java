3aea5048392b10c8de0a188687027148019d2093
MoonCheesez <joeltiojinhon@gmail.com>
2017-04-14 23:04:53 +0800

    Changed test names, updated code to handle errors.

--- sstannouncer/app/src/androidTest/java/sstinc/sstannouncer/RSSParserTest.java
+++ sstannouncer/app/src/androidTest/java/sstinc/sstannouncer/RSSParserTest.java
@@ -22,34 +22,34 @@ import static org.junit.Assert.assertTrue;
 @RunWith(AndroidJUnit4.class)
 public class RSSParserTest {
     @Test
-    public void feed_last_changed_is() {
+    public void feed_last_changed_isDate() {
         XML xml = new XML();
         try {
             xml.fetch("http://studentsblog.sst.edu.sg/feeds/posts/default");
+            Feed feed = RSSParser.parse(xml);
+
+            assertTrue(feed.getLastChanged() != null);
+            assertTrue(feed.getLastChanged() instanceof Date);
+
+            Date now = new Date();
+
+            assertTrue(feed.getLastChanged().before(now));
         } catch (Exception e) {
             fail(e.getMessage());
         }
-        Feed feed = RSSParser.parse(xml);
-
-        assertTrue(feed.getLastChanged() != null);
-        assertTrue(feed.getLastChanged() instanceof Date);
-
-        Date now = new Date();
-
-        assertTrue(feed.getLastChanged().before(now));
     }
 
     @Test
     public void feed_hasCategories() {
         XML xml = new XML();
         try {
-        xml.fetch("http://studentsblog.sst.edu.sg/feeds/posts/default");
+            xml.fetch("http://studentsblog.sst.edu.sg/feeds/posts/default");
+            Feed feed = RSSParser.parse(xml);
+
+            assertFalse(feed.getCategories().isEmpty());
         } catch (Exception e) {
             fail(e.getMessage());
         }
-        Feed feed = RSSParser.parse(xml);
-
-        assertFalse(feed.getCategories().isEmpty());
     }
 
     @Test
@@ -57,35 +57,34 @@ public class RSSParserTest {
         XML xml = new XML();
         try {
             xml.fetch("http://studentsblog.sst.edu.sg/feeds/posts/default");
+            Feed feed = RSSParser.parse(xml);
+
+            assertFalse(feed.getEntries().isEmpty());
         } catch (Exception e) {
             fail(e.getMessage());
         }
-        Feed feed = RSSParser.parse(xml);
-
-        assertFalse(feed.getEntries().isEmpty());
     }
 
     @Test
-    public void feed_entries_notEmpty() {
+    public void feed_entry_data_notEmpty() {
         XML xml = new XML();
         try {
             xml.fetch("http://studentsblog.sst.edu.sg/feeds/posts/default");
+            Feed feed = RSSParser.parse(xml);
+
+            List<Entry> entries = feed.getEntries();
+            for (Entry entry : entries) {
+                assertFalse(entry.getId().isEmpty());
+                assertFalse(entry.getPublished().isEmpty());
+                assertFalse(entry.getLastUpdated().isEmpty());
+                assertFalse(entry.getAuthorName().isEmpty());
+                assertFalse(entry.getBloggerLink().isEmpty());
+
+                assertFalse(entry.getTitle().isEmpty());
+                assertFalse(entry.getContent().isEmpty());
+            }
         } catch (Exception e) {
             fail(e.getMessage());
-        }
-        Feed feed = RSSParser.parse(xml);
-
-        List<Entry> entries = feed.getEntries();
-        for (Entry entry : entries) {
-            assertFalse(entry.getId().isEmpty());
-            assertFalse(entry.getPublished().isEmpty());
-            assertFalse(entry.getLastUpdated().isEmpty());
-            assertFalse(entry.getCategories().isEmpty());
-            assertFalse(entry.getAuthorName().isEmpty());
-            assertFalse(entry.getBloggerLink().isEmpty());
-
-            assertFalse(entry.getTitle().isEmpty());
-            assertFalse(entry.getContent().isEmpty());
         }
     }
 
@@ -100,25 +99,25 @@ public class RSSParserTest {
         XML xml = new XML();
         try {
             xml.fetch("http://studentsblog.sst.edu.sg/feeds/posts/default");
+
+            Feed feed = RSSParser.parse(xml);
+
+            List<Entry> entries = feed.getEntries();
+            for (Entry entry : entries) {
+                // Example id: tag:blogger.com,1999:blog-2263345748458699524.post-351551394302599923
+                String idRegex = "tag:blogger\\.com,\\d{4}:blog-\\d+\\.post-\\d+";
+                assertTrue(validate_with_regex(entry.getId(), idRegex));
+
+                // The regex only matches for numbers, a month like 14 would still be accepted
+                String dateRegex = "\\d{4}(-\\d{2}){2}T\\d{2}(:\\d{2}){2}\\.\\d{3}\\+\\d{2}:\\d{2}";
+                assertTrue(validate_with_regex(entry.getPublished(), dateRegex));
+                assertTrue(validate_with_regex(entry.getLastUpdated(), dateRegex));
+
+                // This is a very basic check, other links may pass
+                assertTrue(entry.getBloggerLink().startsWith("http://"));
+            }
         } catch (Exception e) {
             fail(e.getMessage());
-        }
-
-        Feed feed = RSSParser.parse(xml);
-
-        List<Entry> entries = feed.getEntries();
-        for (Entry entry : entries) {
-            // Example id: tag:blogger.com,1999:blog-2263345748458699524.post-351551394302599923
-            String idRegex = "tag:blogger\\.com,\\d{4}:blog-\\d+\\.post-\\d+";
-            assertTrue(validate_with_regex(entry.getId(), idRegex));
-
-            // The regex only matches for numbers, a month like 14 would still be accepted
-            String dateRegex = "\\d{4}(-\\d{2}){2}T\\d{2}(:\\d{2}){2}\\.\\d{3}\\+\\d{2}:\\d{2}";
-            assertTrue(validate_with_regex(entry.getPublished(), dateRegex));
-            assertTrue(validate_with_regex(entry.getLastUpdated(), dateRegex));
-
-            // This is a very basic check, other links may pass
-            assertTrue(entry.getBloggerLink().startsWith("http://"));
         }
     }
 }
