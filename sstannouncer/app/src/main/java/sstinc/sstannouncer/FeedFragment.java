2e5a10789e09edbdd9d4eef08592a0bb8fb50e15
MoonCheesez <joeltiojinhon@gmail.com>
2017-04-17 14:51:33 +0800

    Added pull to refresh to feed fragment

--- sstannouncer/app/src/main/java/sstinc/sstannouncer/FeedFragment.java
+++ sstannouncer/app/src/main/java/sstinc/sstannouncer/FeedFragment.java
@@ -1,14 +1,19 @@
 package sstinc.sstannouncer;
 
 import android.app.ListFragment;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -42,6 +47,9 @@ public class FeedFragment extends ListFragment implements AdapterView.OnItemClic
         protected void onPostExecute(ArrayList<Entry> entries) {
             super.onPostExecute(entries);
             setListAdapter(new FeedArrayAdapter(getActivity(), entries));
+            if (mSwipeRefreshLayout.isRefreshing()) {
+                mSwipeRefreshLayout.setRefreshing(false);
+            }
         }
     }
 
@@ -54,6 +62,35 @@ public class FeedFragment extends ListFragment implements AdapterView.OnItemClic
         new fetchFeed().execute();
     }
 
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        final View listFragmentView = super.onCreateView(inflater, container, savedInstanceState);
+
+        mSwipeRefreshLayout = new ListFragmentSwipeRefreshLayout(getActivity());
+        mSwipeRefreshLayout.addView(listFragmentView,
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+        mSwipeRefreshLayout.setLayoutParams(
+                new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT));
+
+        mSwipeRefreshLayout.setOnRefreshListener(
+            new SwipeRefreshLayout.OnRefreshListener() {
+                @Override
+                public void onRefresh() {
+                    new fetchFeed().execute();
+                }
+            }
+        );
+
+        return mSwipeRefreshLayout;
+    }
+
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
         Entry entry = (Entry) getListAdapter().getItem(i);
@@ -61,5 +98,17 @@ public class FeedFragment extends ListFragment implements AdapterView.OnItemClic
         Intent intent = new Intent(getActivity(), EntryActivity.class);
         intent.putExtra(EntryActivity.ENTRY_EXTRA, entry);
         startActivity(intent);
+    }
+
+    private class ListFragmentSwipeRefreshLayout extends SwipeRefreshLayout {
+        public ListFragmentSwipeRefreshLayout(Context context) { super(context); }
+
+        @Override
+        public boolean canChildScrollUp() {
+            final ListView listView = getListView();
+            return listView.getVisibility() == View.VISIBLE &&
+                    ViewCompat.canScrollVertically(listView, -1);
+        }
+
     }
 }
