bed16649b2999cc36d6fcfc42a230e959f8e46d8
MoonCheesez <joeltiojinhon@gmail.com>
2017-04-17 14:30:13 +0800

    Moved fetching of XML to AsyncTask

--- sstannouncer/app/src/main/java/sstinc/sstannouncer/FeedFragment.java
+++ sstannouncer/app/src/main/java/sstinc/sstannouncer/FeedFragment.java
@@ -2,11 +2,13 @@ package sstinc.sstannouncer;
 
 import android.app.ListFragment;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ListAdapter;
 
 import java.util.ArrayList;
 
@@ -18,24 +20,38 @@ import sstinc.sstannouncer.Feed.XML;
 public class FeedFragment extends ListFragment implements AdapterView.OnItemClickListener {
     public FeedFragment() {}
 
+    private class fetchFeed extends AsyncTask<Void, Void, ArrayList<Entry>> {
+        @Override
+        protected ArrayList<Entry> doInBackground(Void... voids) {
+            ArrayList<Entry> entries;
+
+            try {
+                XML xml = new XML();
+                xml.fetch(getString(R.string.blog_rss_url));
+                Feed feed = RSSParser.parse(xml);
+                entries = feed.getEntries();
+            } catch (Exception e) {
+                Log.e(this.getClass().getName(), e.getMessage());
+                entries = new ArrayList<>();
+            }
+
+            return entries;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Entry> entries) {
+            super.onPostExecute(entries);
+            setListAdapter(new FeedArrayAdapter(getActivity(), entries));
+        }
+    }
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        ArrayList<Entry> entries;
-
-        try {
-            XML xml = new XML();
-            xml.fetch(getString(R.string.blog_rss_url));
-            Feed feed = RSSParser.parse(xml);
-            entries = feed.getEntries();
-        } catch (Exception e) {
-            Log.e(this.getClass().getName(), e.getMessage());
-            entries = new ArrayList<>();
-        }
-
-        setListAdapter(new FeedArrayAdapter(getActivity(), entries));
         getListView().setOnItemClickListener(this);
+
+        new fetchFeed().execute();
     }
 
     @Override
