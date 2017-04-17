930686410b47eed7c78b678c53ce439d81d3bc56
MoonCheesez <joeltiojinhon@gmail.com>
2017-04-17 17:20:04 +0800

    Updated AsyncTask of FeedFragment to fetch feed when modified only

--- sstannouncer/app/src/main/java/sstinc/sstannouncer/FeedFragment.java
+++ sstannouncer/app/src/main/java/sstinc/sstannouncer/FeedFragment.java
@@ -3,6 +3,7 @@ package sstinc.sstannouncer;
 import android.app.ListFragment;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -15,6 +16,8 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 import sstinc.sstannouncer.Feed.Entry;
@@ -25,33 +28,8 @@ import sstinc.sstannouncer.Feed.XML;
 public class FeedFragment extends ListFragment implements AdapterView.OnItemClickListener {
     public FeedFragment() {}
 
-    private class fetchFeed extends AsyncTask<Void, Void, ArrayList<Entry>> {
-        @Override
-        protected ArrayList<Entry> doInBackground(Void... voids) {
-            ArrayList<Entry> entries;
-
-            try {
-                XML xml = new XML();
-                xml.fetch(getString(R.string.blog_rss_url));
-                Feed feed = RSSParser.parse(xml);
-                entries = feed.getEntries();
-            } catch (Exception e) {
-                Log.e(this.getClass().getName(), e.getMessage());
-                entries = new ArrayList<>();
-            }
-
-            return entries;
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<Entry> entries) {
-            super.onPostExecute(entries);
-            setListAdapter(new FeedArrayAdapter(getActivity(), entries));
-            if (mSwipeRefreshLayout.isRefreshing()) {
-                mSwipeRefreshLayout.setRefreshing(false);
-            }
-        }
-    }
+    private static final String FEED_FRAGMENT_PREFERENCE = "feed_fragment_preference";
+    private static final String LAST_MODIFIED_PREFERENCE = "last_modified";
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -59,7 +37,7 @@ public class FeedFragment extends ListFragment implements AdapterView.OnItemClic
 
         getListView().setOnItemClickListener(this);
 
-        new fetchFeed().execute();
+        new fetchNewFeed().execute(true);
     }
 
     private SwipeRefreshLayout mSwipeRefreshLayout;
@@ -83,7 +61,7 @@ public class FeedFragment extends ListFragment implements AdapterView.OnItemClic
             new SwipeRefreshLayout.OnRefreshListener() {
                 @Override
                 public void onRefresh() {
-                    new fetchFeed().execute();
+                    new fetchNewFeed().execute();
                 }
             }
         );
@@ -98,6 +76,70 @@ public class FeedFragment extends ListFragment implements AdapterView.OnItemClic
         Intent intent = new Intent(getActivity(), EntryActivity.class);
         intent.putExtra(EntryActivity.ENTRY_EXTRA, entry);
         startActivity(intent);
+    }
+
+    // Fetches and updates the feed only if it has been modified
+    private class fetchNewFeed extends AsyncTask<Boolean, Void, ArrayList<Entry>> {
+        private boolean feedHasBeenModified() {
+            boolean feedHasBeenModified = true;
+
+            SharedPreferences preferences = getActivity().getSharedPreferences(
+                    FEED_FRAGMENT_PREFERENCE, Context.MODE_PRIVATE);
+            String lastModified = preferences.getString(LAST_MODIFIED_PREFERENCE, "");
+
+            HttpURLConnection urlConnection = null;
+            try {
+                URL url = new URL(getResources().getString(R.string.blog_rss_url));
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("HEAD");
+
+                feedHasBeenModified = lastModified.equals(urlConnection.getHeaderField("Last-Modified"));
+
+                urlConnection.getInputStream().close();
+            } catch (Exception e) {
+                Log.e(this.getClass().getName(), e.getMessage());
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+
+            return feedHasBeenModified;
+        }
+
+        @Override
+        protected ArrayList<Entry> doInBackground(Boolean... force_bools) {
+            // true if first element of force_bools is true else false
+            boolean force = force_bools.length > 0 && force_bools[0];
+
+            ArrayList<Entry> entries;
+            if (force || feedHasBeenModified()) {
+                try {
+                    XML xml = new XML();
+                    xml.fetch(getString(R.string.blog_rss_url));
+                    Feed feed = RSSParser.parse(xml);
+                    entries = feed.getEntries();
+                } catch (Exception e) {
+                    Log.e(this.getClass().getName(), e.getMessage());
+                    entries = new ArrayList<>();
+                }
+            } else {
+                entries = new ArrayList<>();
+            }
+
+            return entries;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Entry> entries) {
+            super.onPostExecute(entries);
+            if (!entries.isEmpty()) {
+                setListAdapter(new FeedArrayAdapter(getActivity(), entries));
+            }
+            if (mSwipeRefreshLayout.isRefreshing()) {
+                mSwipeRefreshLayout.setRefreshing(false);
+            }
+        }
     }
 
     private class ListFragmentSwipeRefreshLayout extends SwipeRefreshLayout {
