f89aa2a1d82c6b2e74ac7b3dfcce9caa42fc7075
MoonCheesez <joeltiojinhon@gmail.com>
2017-04-17 21:35:46 +0800

    Removed unneeded code and file

--- sstannouncer/app/src/main/java/sstinc/sstannouncer/MainActivity.java
+++ sstannouncer/app/src/main/java/sstinc/sstannouncer/MainActivity.java
@@ -3,8 +3,6 @@ package sstinc.sstannouncer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,24 +14,5 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.toolbar_all);
         setSupportActionBar(toolbar);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
