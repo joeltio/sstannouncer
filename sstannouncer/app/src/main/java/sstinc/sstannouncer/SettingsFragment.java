2e8911ba4803309c095c290b34bbb7229380e807
MoonCheesez <joeltiojinhon@gmail.com>
2017-04-24 17:36:51 +0800

    Added refresh rate setting in UI

--- sstannouncer/app/src/main/java/sstinc/sstannouncer/SettingsFragment.java
+++ sstannouncer/app/src/main/java/sstinc/sstannouncer/SettingsFragment.java
@@ -1,14 +1,42 @@
 package sstinc.sstannouncer;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
-public class SettingsFragment extends PreferenceFragment {
+import java.util.Date;
+
+import sstinc.sstannouncer.event.Event;
+import sstinc.sstannouncer.event.EventController;
+
+public class SettingsFragment extends PreferenceFragment
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.pref_general);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(
+                getActivity());
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+        addPreferencesFromResource(R.xml.pref_data_usage);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        if (s.equals(getResources().getString(R.string.pref_refresh_rate))) {
+            int refreshRateMilliseconds = Integer.parseInt(sharedPreferences.getString(
+                    getResources().getString(R.string.pref_refresh_rate), ""))*1000;
+
+            if (FeedFragment.eventController == null) {
+                FeedFragment.eventController = new EventController();
+            }
+            // frequency in milliseconds
+            double frequency = 1/refreshRateMilliseconds;
+            FeedFragment.eventController.raise(new Event(getString(
+                    R.string.event_resource_service_change_frequency), new Date(), Double.toString(frequency)));
+        }
     }
 }
