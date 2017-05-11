b6810c284a4a6ef2f09eb22fa0a71e53f7400103
Zhu Zhan Yan <program.nom@gmail.com>
2017-05-11 09:20:22 +0800

    Git added tests for new notification system.

--- sstannouncer/app/src/androidTest/java/com/sst/anouncements/EventSystemTest.java
+++ sstannouncer/app/src/androidTest/java/com/sst/anouncements/EventSystemTest.java
@@ -1,11 +1,20 @@
 package com.sst.anouncements;
 
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
 import com.sst.anouncements.android.AndroidEventAdaptor;
+import com.sst.anouncements.android.AndroidNotificationAdaptor;
 import com.sst.anouncements.event.Event;
 import com.sst.anouncements.event.EventController;
 import com.sst.anouncements.event.EventHandler;
+import com.sst.anouncements.event.FeedEventInterpreter;
+import com.sst.anouncements.event.ResourceEventInterpreter;
+import com.sst.anouncements.resource.HTTPResourceAcquirer;
+import com.sst.anouncements.resource.Resource;
+import com.sst.anouncements.service.ResourceService;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -91,8 +100,8 @@ public class EventSystemTest
 
         try
         {
-            Thread.sleep(1); //Event Processing Must be done within 1ms
-        }catch(InterruptedException exp){}
+            Thread.sleep(10); //Event Processing Must be done within 10ms
+        }catch(InterruptedException exp){};
 
         assertTrue(eventStatus);
 
@@ -100,4 +109,82 @@ public class EventSystemTest
         remoteEventAdaptor.disconnect();
     }
 
+    @Test
+    public void testResourceEventInterpreter()
+    {
+        EventController eventController = new EventController();
+        ResourceEventInterpreter resourceEventInterpreter =
+                new ResourceEventInterpreter(eventController, null);
+
+        resourceEventInterpreter.unBind();
+        resourceEventInterpreter.bind(eventController);
+
+        final Resource resource = new
+                Resource("http://test-sst.blogspot.sg/feeds/posts/default", new Date(0), "");
+        HTTPResourceAcquirer resourceAcquirer = new HTTPResourceAcquirer();
+        resourceAcquirer.retrieve(resource);
+
+        eventStatus = false;
+        eventController.listen(this.toString(),
+                        FeedEventInterpreter.getFeedChangedEvent().getIdentifier(),
+                        new EventHandler() {
+                            @Override
+                            public void handle(Event event) {
+                                assertEquals(event.getIdentifier(),
+                                        FeedEventInterpreter.getFeedChangedEvent().getIdentifier());
+                                assertEquals(new Resource(event.getData()), resource);
+                                eventStatus = true;
+
+                            }
+                        });
+
+        Event resourceChangedEvent = new
+                Event(ResourceService.getResourceChangedEvent().getIdentifier(),
+                new Date(),
+                resource.toString());
+        eventController.raise(resourceChangedEvent);
+
+        try
+        {
+            Thread.sleep(10); //Event Processing must be done in 10ms
+        }catch(InterruptedException exp){};
+
+        assertTrue(eventStatus);
+
+        resourceEventInterpreter.unBind();
+    }
+
+    @Test
+    public void testFeedEventInterpreter()
+    {
+        EventController eventController = new EventController();
+        Context context = InstrumentationRegistry.getTargetContext();
+        AndroidNotificationAdaptor notificationAdaptor =
+                new AndroidNotificationAdaptor(context, R.drawable.notifcation_icon);
+
+        final Resource resource = new
+                Resource("http://test-sst.blogspot.sg/feeds/posts/default", new Date(0), "");
+        HTTPResourceAcquirer resourceAcquirer = new HTTPResourceAcquirer();
+        resourceAcquirer.retrieve(resource);
+
+        FeedEventInterpreter feedEventInterpreter = new FeedEventInterpreter(eventController,
+                notificationAdaptor);
+
+        feedEventInterpreter.unBind();
+        feedEventInterpreter.bind(eventController);
+
+        Event feedChangedEvent =
+                new Event(FeedEventInterpreter.getFeedChangedEvent().getIdentifier(),
+                        new Date(0),
+                        resource.toString());
+
+
+        eventController.raise(feedChangedEvent);
+
+        try
+        {
+            Log.d("TFeedEventInterpreter", "Check for notification in notification drawer.");
+            Thread.sleep(1000 * 5); //Delay to show the lack in notification.
+        }catch(InterruptedException exp){};
+    }
 }
