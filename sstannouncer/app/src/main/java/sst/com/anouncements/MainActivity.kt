7e662f58f13d7067025790fcd471340de37c3f02
Joel Tio <joeltiojinhon@gmail.com>
2019-06-22 23:41:42 +0800

    Add FeedFragment as main fragment

--- sstannouncer/app/src/main/java/sst/com/anouncements/MainActivity.kt
+++ sstannouncer/app/src/main/java/sst/com/anouncements/MainActivity.kt
@@ -2,11 +2,18 @@ package sst.com.anouncements
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import sst.com.anouncements.feed.FeedFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val mainFragment = FeedFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_fragment, mainFragment, mainFragment.tag)
+            .commit()
+
         setContentView(R.layout.activity_main)
     }
 }
