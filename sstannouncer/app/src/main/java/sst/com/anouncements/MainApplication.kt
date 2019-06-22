3739733ae2e8bf8d7a373e246ddf370249570e7d
Joel Tio <joeltiojinhon@gmail.com>
2019-06-22 23:01:56 +0800

    Initialise Koin in application

---
+++ sstannouncer/app/src/main/java/sst/com/anouncements/MainApplication.kt
@@ -0,0 +1,21 @@
+package sst.com.anouncements
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidFileProperties
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MainApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+
+            androidContext(this@MainApplication)
+
+            androidFileProperties()
+        }
+    }
+}
