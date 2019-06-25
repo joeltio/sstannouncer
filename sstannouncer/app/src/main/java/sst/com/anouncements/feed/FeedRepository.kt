18bc6074bf0d00375cf4bcd1b1d2fc434651c882
Joel Tio <joeltiojinhon@gmail.com>
2019-06-25 23:13:37 +0800

    Update FeedRepository to use database module

--- sstannouncer/app/src/main/java/sst/com/anouncements/feed/FeedRepository.kt
+++ sstannouncer/app/src/main/java/sst/com/anouncements/feed/FeedRepository.kt
@@ -1,14 +1,22 @@
 package sst.com.anouncements.feed
 
 import sst.com.anouncements.feed.data.Feed
+import sst.com.anouncements.feed.data.database.FeedDAL
 import sst.com.anouncements.feed.data.parser.parse
 import sst.com.anouncements.http.Webservice
 
 class FeedRepository(
-    private val webservice: Webservice
+    private val webservice: Webservice,
+    private val feedDAL: FeedDAL
 ) {
     fun getFeed(feedURL: String): Feed {
-        val data: String = webservice.get(feedURL)
-        return parse(data)
+        if (!feedDAL.hasFeed(feedURL)) {
+            val data: String = webservice.get(feedURL)
+            feedDAL.overwriteFeed(parse(data), feedURL)
+        }
+
+        // Always get only from DAL, so that the data shown is
+        // always representative of the database
+        return feedDAL.getFeed(feedURL)
     }
 }
--- sstannouncer/app/src/main/java/sst/com/anouncements/feed/feedModule.kt
+++ sstannouncer/app/src/main/java/sst/com/anouncements/feed/feedModule.kt
@@ -1,12 +1,24 @@
 package sst.com.anouncements.feed
 
+import androidx.room.Room
+import org.koin.android.ext.koin.androidApplication
 import org.koin.core.module.Module
 import org.koin.dsl.bind
 import org.koin.dsl.module
+import sst.com.anouncements.feed.data.database.FeedDAL
+import sst.com.anouncements.feed.data.database.room.FeedDatabase
+import sst.com.anouncements.feed.data.database.room.RoomDAL
 import sst.com.anouncements.http.DefaultWebservice
 import sst.com.anouncements.http.Webservice
 
 val feedModule: Module = module {
+    // Use the default webservice for the repository
     factory { DefaultWebservice() } bind Webservice::class
-    single { FeedRepository(get()) }
+    // Use the RoomDAL implementation for storing and retrieving data
+    single {
+        Room.databaseBuilder(androidApplication(), FeedDatabase::class.java, "feed-db").build()
+    }
+    factory { RoomDAL(get()) } bind FeedDAL::class
+
+    single { FeedRepository(get(), get()) }
 }
