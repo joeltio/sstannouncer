66e16b457415fdd75b65d8737b9ac94161be2467
Joel Tio <joeltiojinhon@gmail.com>
2020-07-16 23:01:45 +0800

    Update PostFragment to show content

--- sstannouncer/app/src/main/java/sst/com/anouncements/feed/ui/PostFragment.kt
+++ sstannouncer/app/src/main/java/sst/com/anouncements/feed/ui/PostFragment.kt
@@ -1,10 +1,12 @@
 package sst.com.anouncements.feed.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_post.*
 import sst.com.anouncements.R
 import sst.com.anouncements.feed.model.Entry
 
@@ -18,10 +20,38 @@ class PostFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // 1. Add back button
+        // Add content to the fragment
+        title_text_view.text = entry.title
+        author_text_view.text = entry.authorName
+        date_text_view.text = entry.relativePublishedDate
 
-        // 2. Add contents from argument to view
+        // Add custom CSS and JS to make images fit within the window better
+        val customHTML = """
+            <style>
+                img {
+                    display: inline;
+                    height: auto;
+                    max-width: 100%;
+                }
+            </style>
+            <script>
+                document.addEventListener('DOMContentLoaded', function(event) {
+                    var imgs = document.getElementsByTagName('img');
+                    for (var i=0; i<imgs.length; i++) {
+                        console.log(imgs[i].parentElement.style.marginLeft);
+                        imgs[i].parentElement.style.marginLeft = '0px';
+                    }
+                });
+            </script>
+        """.trimIndent()
+        // loadData cannot be used here as it is breaks when special characters are not escaped in
+        // the content. See https://stackoverflow.com/a/11926466/4428725
+        content_web_view.loadDataWithBaseURL(null, customHTML + entry.content, "text/html; charset=utf-8", "UTF-8", null)
 
-        // 3. View in browser
+        // It is acceptable to enable JavaScript here as the source is deemed trustworthy
+        // If not, anyone viewing the blog will also get XSSed
+        @SuppressLint("SetJavaScriptEnabled")
+        content_web_view.settings.javaScriptEnabled = true
+        content_web_view.settings.loadsImagesAutomatically = true
     }
 }
