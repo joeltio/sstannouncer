68f52a682a4d23c9353f3e9840125ee47311ae72
Joel Tio <joeltiojinhon@gmail.com>
2020-07-18 17:20:38 +0800

    Add background worker

--- sstannouncer/app/src/main/java/sst/com/anouncements/MainApplication.kt
+++ sstannouncer/app/src/main/java/sst/com/anouncements/MainApplication.kt
@@ -1,16 +1,11 @@
 package sst.com.anouncements
 
 import android.app.Application
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.os.Build
-import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidFileProperties
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import sst.com.anouncements.feed.feedModule
-import sst.com.anouncements.notification.NotificationService
 
 class MainApplication : Application() {
     override fun onCreate() {
@@ -25,8 +20,5 @@ class MainApplication : Application() {
 
             modules(appModule, feedModule)
         }
-
-        val notificationService: NotificationService by inject()
-        notificationService.setup()
     }
 }
---
+++ sstannouncer/app/src/main/java/sst/com/anouncements/feed/worker/UpdateFeedWorker.kt
@@ -0,0 +1,45 @@
+package sst.com.anouncements.feed.worker
+
+import android.content.Context
+import androidx.work.*
+import org.koin.core.KoinComponent
+import org.koin.core.inject
+import sst.com.anouncements.feed.data.FeedRepository
+import sst.com.anouncements.notification.NotificationService
+import java.util.concurrent.TimeUnit
+
+const val UPDATE_FEED_WORK_NAME = "UPDATE_FEED_PERIODICAL"
+const val WORKER_INPUT_FEED_URL = "feedURL"
+
+class UpdateFeedWorker(
+    appContext: Context,
+    workerParams: WorkerParameters
+) : Worker(appContext, workerParams), KoinComponent {
+    companion object {
+        fun createWorkRequest(feedURL: String): PeriodicWorkRequest {
+            val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build()
+
+            val inputData = Data.Builder()
+            inputData.putString(WORKER_INPUT_FEED_URL, feedURL)
+
+            return PeriodicWorkRequestBuilder<UpdateFeedWorker>(15, TimeUnit.MINUTES)
+                .setInputData(inputData.build())
+                .setConstraints(constraints)
+                .build()
+        }
+    }
+    override fun doWork(): Result {
+        val feedURL = inputData.getString(WORKER_INPUT_FEED_URL)!!
+        val feedRepository: FeedRepository by inject()
+        val notificationService: NotificationService by inject()
+
+        val newEntries = feedRepository.getNewEntries(feedURL)
+        newEntries.map {
+            notificationService.pushNewEntry(it)
+        }
+
+        return Result.success()
+    }
+}
